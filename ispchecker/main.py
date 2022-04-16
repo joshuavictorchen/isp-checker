4e497a9a31491983354396f2732278dd20a14e8f
neutronsupervisor <joshuavictorchen@gmail.com>
2022-04-16 14:11:19 -0400

    add option for continuous runs vs. one-off execution

--- ispchecker/main.py
+++ ispchecker/main.py
@@ -1,3 +1,4 @@
+import sys
 import traceback
 from ispchecker import tools as t
 from ispchecker.address import Address
@@ -12,7 +13,22 @@ DIVIDER = (
 
 def main():
 
-    # rudimentary CLI that allows users to start the program and continuously check different addresses
+    # rudimentary CLI - outputs just printed to the console for now
+
+    # if no arg is provided, then allows users to start the program and continuously check different addresses/URLs
+    # if args are provided, then simply executes a one-off ISP check
+    args = sys.argv
+
+    if len(args) > 1:
+
+        # create a string from the sys args list
+        # the first element in list is the name of the calling function; it is ignored
+        arg = " ".join(sys.argv[1:])
+
+        # check the address and exit the program
+        check_address(arg)
+        sys.exit()
+
     while True:
 
         print(DIVIDER)
@@ -31,17 +47,7 @@ def main():
 
             # check the address (input checking/cleaning routines TBD)
             else:
-
-                # instantiate an Address
-                a = Address()
-
-                # load in the argument
-                t.print_divider()
-                a.parse_address(arg)
-                t.print_recursive_dict(a.address)
-
-                # check for isp availability
-                a.check_isps([Spectrum(), CenturyLink(), Verizon()])
+                check_address(arg)
 
         except Exception:
 
@@ -50,6 +56,22 @@ def main():
                 "\n ERROR: prototype interface encountered the following exception:\n"
             )
             [print(f"   {i}") for i in traceback.format_exc().split("\n")]
+
+
+def check_address(arg):
+
+    # check ISP availability for the supplied argument (address or supported URL)
+
+    # instantiate an Address
+    a = Address()
+
+    # load in the argument
+    t.print_divider()
+    a.parse_address(arg)
+    t.print_recursive_dict(a.address)
+
+    # check for ISP availability
+    a.check_isps([Spectrum(), CenturyLink(), Verizon()])
 
 
 if __name__ == "__main__":
